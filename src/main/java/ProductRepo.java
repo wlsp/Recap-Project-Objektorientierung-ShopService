87837b9266a3189caff27fe04d031c68c15a4b5d
wlsp <wlsp@outlook.de>
2025-04-14 16:46:41 +0200

    -fixed Test for OrderStatus -added Optional to getProductById -added exception to addOrder function in ShopService

--- src/main/java/ProductRepo.java
+++ src/main/java/ProductRepo.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class ProductRepo {
     private List<Product> products;
@@ -13,13 +14,8 @@ public class ProductRepo {
         return products;
     }
 
-    public Product getProductById(String id) {
-        for (Product product : products) {
-            if (product.id().equals(id)) {
-                return product;
-            }
-        }
-        return null;
+    public Optional<Product> getProductById(String id) {
+        return products.stream().filter(product -> product.id().equals(id)).findFirst();
     }
 
     public Product addProduct(Product newProduct) {
--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,24 +1,32 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public class ShopService {
     private ProductRepo productRepo = new ProductRepo();
     private OrderRepo orderRepo = new OrderMapRepo();
 
-    public Order addOrder(List<String> productIds) {
+    public Order addOrder(List<String> productIds) throws NoProductFoundException {
         List<Product> products = new ArrayList<>();
+
         for (String productId : productIds) {
-            Product productToOrder = productRepo.getProductById(productId);
-            if (productToOrder == null) {
-                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
-                return null;
+            Optional<Product> productToOrder = productRepo.getProductById(productId);
+            if (productToOrder.isEmpty()) {
+                throw new NoProductFoundException("No product with the id: " + productId + " found. Try again!");
+
             }
-            products.add(productToOrder);
+            products.add(productToOrder.get());
         }
 
         Order newOrder = new Order(UUID.randomUUID().toString(), products, OrderStatus.PROCESSING);
 
         return orderRepo.addOrder(newOrder);
+    }
+
+    public List<Order> getOrdersByStatus(OrderStatus status) {
+        return orderRepo.getOrders().stream()
+                .filter(order -> order.status() == status)
+                .toList();
     }
 }
